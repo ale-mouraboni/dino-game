2755f83aeb516476d305a082d461b58698835e76
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-03 19:36:33 -0300

    function jump complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,23 @@ function jump() {
     let position = 0
 
     let upInterval = setInterval(() => {
-        position += 20
+        if (position >= 150) {
+            clearInterval(upInterval)
 
-        dino.style.bottom = position + 'px'
+            // descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval)
+                } else {
+                    position -= 20
+                    dino.style.bottom = position + 'px'
+                }
+            }, 20)
+        } else {
+            // subindo
+            position += 20
+            dino.style.bottom = position + 'px'
+        }
     }, 20)
 }
 
