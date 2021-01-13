00e898da3d5e5672bd4910d08efe31fe35cbf62e
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-13 19:05:46 -0300

    new index

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,7 @@ function createCactus() {
             // Game over
             clearInterval(leftTimer);
             isGameOver = true;
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1></br><a href="classic.html" class="jogar-novamente">Jogar Novamente</a>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
